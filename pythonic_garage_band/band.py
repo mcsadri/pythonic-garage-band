5653908fc0db14eaf7287f9ace1401aa5fd47e7d
Manuch Sadri <colonquebang@hotmail.com>
2023-05-23 13:54:25 -0700

    scaffolds classes with basic functionality to pass first unit test for guitarist

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,18 +1,52 @@
 class Band:
-    pass
+    def __init__(self, name="unknown", members=[]):
+        self.name = name
+        self.members = members
+
+    def play_solos(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def to_list(self):
+        pass
+
 
 class Musician:
-    def __init__(self, name="unknown"):
+    def __init__(self, name="", instrument=""):
         self.name = name
+        self.instrument = instrument
     pass
 
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        pass
+
+    def get_instrument(self):
+        pass
+
+    def play_solo(self):
+        pass
+
+
 class Guitarist(Musician):
-    instrument = "guitar"
-    solo = "face melting guitar solo"
-    pass
+    def __init__(self, name):
+        super().__init__(name, "guitar")
+
 
 class Bassist(Musician):
     pass
 
+
 class Drummer(Musician):
+    pass
+
+
+if __name__ == "__main__":
     pass
--- tests/test_band.py
+++ tests/test_band.py
@@ -9,7 +9,7 @@ from pythonic_garage_band.band import (
 )
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_guitarist_str():
     joan = Guitarist("Joan Jett")
     actual = str(joan)
